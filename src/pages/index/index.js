ca0673b28d1de8e339d8484432e24d4b47402d11
leetx <1982492725@qq.com>
2020-11-11 14:48:15 +0800

    react 打包环境

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,43 +1,8 @@
-import css from "./index.less";
-import imgSrc from "../../images/webpackImg.jpeg";
-
-import counter from "./counter";
-import number from "./number";
-
-console.log("====================================");
-console.log("Index Page");
-console.log("====================================");
-
-const imgTag = new Image();
-imgTag.src = imgSrc;
-document.getElementById("app").appendChild(imgTag);
-
-
-const arr = [new Promise(() => {}), new Promise(() => {})];
-arr.map(item => {
-  console.log(item);
-});
-
-
-// ==========================================
-
-const btn = document.createElement("button");
-btn.innerHTML = "新增";
-document.body.appendChild(btn);
-
-btn.onclick = function () {
-  const div = document.createElement("div");
-  div.innerHTML = "item";
-  document.body.appendChild(div);
-};
-
-
-counter();
-number();
-
-if (module.hot) {
-  module.hot.accept("./number", function() {
-    document.body.removeChild(document.getElementById("number"));
-    number();
-  });
+import React, { Component } from "react";
+import ReactDom from "react-dom";
+class App extends Component {
+  render() {
+    return <div>hello world</div>;
+  }
 }
+ReactDom.render(<App />, document.getElementById("app"));
